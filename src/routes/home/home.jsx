4d63c691bd4c9d95e00f97664e307a4ca2b366c9
jameswquinn <jameswquinn@me.com>
2019-08-29 21:19:28 +0100

    update

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -4,6 +4,15 @@ import "./styles.css"
 import CoverVid from "../../components/Covervid/covervid";
 import ViewportIcon from "../../components/ViewportIcon/viewportIcon"
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isMobile: false,
+            videoSrc: "http://techslides.com/demos/sample-videos/small.mp4",
+            imgSrc: require("../../img/cropped_landing_page.png").src
+
+        };
+    }
 
     render() {
         return (
